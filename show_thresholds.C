dad2efa6454c3be4e84355fb8b6ca89689cbc5c2
Roman Dzhygadlo <r.dzhygadlo@gsi.de>
2015-05-07 12:57:06 +0200

    plot difference of thresholds

--- show_thresholds.C
+++ show_thresholds.C
@@ -3,6 +3,7 @@
 const Int_t tdcmax=10000;
 Int_t tdcnum=20;
 Int_t tdcmap[tdcmax];
+Int_t tharr[tdcmax];
 TString trbsid[20] ={"2000","2001","2002","2003","2004","2005","2006","2007","2008","2009",
 		     "2010","2011","2012","2013","2014","2015",
 		     "2016","2017","2018","2019"};
@@ -22,48 +23,97 @@ void CreateMap(){
 }
 
 
-void show_thresholds(TString infile1="thresholds.thr", TString infile2=""){
+void show_thresholds(TString infile1="thresholds.thr", TString infile2="",Int_t log = 0){
   fSavePath = "auto";
   CreateMap();
   initDigi();
-  
-  ifstream in;
-  in.open(infile1.Data());
+
+  Double_t min(1000000),max(0);
   TString s, stdc,sth,schain;
   Int_t ch, tdc, th, chain;
-  Double_t min(1000000),max(0);
+  ifstream in;
+
+  if(infile2 != ""){
+    in.open(infile2.Data());
   
-  while (1) {
-    in >> s >> s >> s >> stdc >> s >> schain >> s >> ch >> s >>sth >> s >> s;
-    if (!in.good()) break;
+    while (1) {
+      in >> s >> s >> s >> stdc >> s >> schain >> s >> ch >> s >>sth >> s >> s;
+      if (!in.good()) break;
     
-    stdc = stdc.Strip(TString::kTrailing,',');
-    tdc = TString::BaseConvert(stdc,16,10).Atoi();
+      stdc = stdc.Strip(TString::kTrailing,',');
+      tdc = TString::BaseConvert(stdc,16,10).Atoi();
 
-    schain = schain.Strip(TString::kLeading,'0');
-    schain = schain.Strip(TString::kTrailing,',');
-    chain = schain.Atoi();
+      schain = schain.Strip(TString::kLeading,'0');
+      schain = schain.Strip(TString::kTrailing,',');
+      chain = schain.Atoi();
     
-    sth = sth.Strip(TString::kTrailing,',');
-    th = TString::BaseConvert(sth,16,10).Atoi();
-    if(th>max) max = th;
-    if(th<min) min = th;
+      sth = sth.Strip(TString::kTrailing,',');
+      th = TString::BaseConvert(sth,16,10).Atoi();
+      if(th>max) max = th;
+      if(th<min) min = th;
     
-    Int_t tdcSeq = tdcmap[tdc];
-    Int_t channel= tdcSeq*48 + chain*16 + ch;
-    Int_t mcp = channel/64;
-    Int_t pix = channel%64;
-    Int_t col = pix/2 - 8*(pix/16);
-    Int_t row = pix%2 + 2*(pix/16);
-    fhDigi[mcp]->Fill(col,row,th);
-
-    //std::cout<<"mcp  "<<mcp << " tdcSeq  " <<tdcSeq << " col  "<< col << "  row "<< row<<std::endl;
+      Int_t tdcSeq = tdcmap[tdc];
+      Int_t channel= tdcSeq*48 + chain*16 + ch;
+      tharr[channel]=th;
+    }
+    in.close();
   }
-  
-  drawDigi("m,p,v\n",3,max,min);
+
+  if(infile1 != ""){
+    if(infile2 != ""){
+      max=-10000000;
+      min=10000000;
+    }
+    in.open(infile1.Data());  
+    while (1) {
+      in >> s >> s >> s >> stdc >> s >> schain >> s >> ch >> s >>sth >> s >> s;
+      if (!in.good()) break;
     
+      stdc = stdc.Strip(TString::kTrailing,',');
+      tdc = TString::BaseConvert(stdc,16,10).Atoi();
+
+      schain = schain.Strip(TString::kLeading,'0');
+      schain = schain.Strip(TString::kTrailing,',');
+      chain = schain.Atoi();
+    
+      sth = sth.Strip(TString::kTrailing,',');
+      th = TString::BaseConvert(sth,16,10).Atoi();
+      if(th>max) max = th;
+      if(th<min) min = th;
+    
+      Int_t tdcSeq = tdcmap[tdc];
+      Int_t channel= tdcSeq*48 + chain*16 + ch;
+      Int_t mcp = channel/64;
+      Int_t pix = channel%64;
+      Int_t col = pix/2 - 8*(pix/16);
+      Int_t row = pix%2 + 2*(pix/16);
+      fhDigi[mcp]->Fill(col,row,th-tharr[channel]);
+
+      if(infile2 != ""){
+	th = th-tharr[channel];
+	if(th>max) max = th;
+	if(th<min) min = th;
+      }
+      //std::cout<<"mcp  "<<mcp << " tdcSeq  " <<tdcSeq << " col  "<< col << "  row "<< row<<std::endl;
+    }
+    in.close();
+  }
+
+  if(log) gStyle->SetOptLogz();
+  drawDigi("m,p,v\n",3,max,min);
+  
   TPaletteAxis *pal = new TPaletteAxis(0.90,0.1,0.94,0.90,fhDigi[0]);   
   cDigi->cd();
   pal->Draw();
-
+  
+  // TClass *cl = gClient->GetRoot()->IsA();
+  // l = cl->GetMenuList();
+  
+  // n = new TClassMenuItem(TClassMenuItem::kPopupUserFunction,cl,
+  // 			 "test no 1","poptest1",0,"int,int");
+  // l->AddFirst(n);
+   
+  //TGMenuBar *fMenuBar = new TGMenuBar(gClient->GetRoot(),100,20,kHorizontalFrame);
+  //fMenuBar->AddPopup("UUUUZ");
+  
 }
