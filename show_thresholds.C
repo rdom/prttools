146f5293c39f12e28d90f62d698c5022327ffdcd
Roman Dzhygadlo <r.dzhygadlo@gsi.de>
2015-05-07 09:27:14 +0200

    fix mcp indexing

--- show_thresholds.C
+++ show_thresholds.C
@@ -50,14 +50,14 @@ void show_thresholds(TString infile1="thresholds.thr", TString infile2=""){
     if(th<min) min = th;
     
     Int_t tdcSeq = tdcmap[tdc];
-    Int_t mcp = tdcSeq*3/4.;
     Int_t channel= tdcSeq*48 + chain*16 + ch;
+    Int_t mcp = channel/64;
     Int_t pix = channel%64;
     Int_t col = pix/2 - 8*(pix/16);
     Int_t row = pix%2 + 2*(pix/16);
     fhDigi[mcp]->Fill(col,row,th);
 
-    //std::cout<<"mcp  "<<mcp << " tdcSeq  " <<tdcSeq << " col  "<< col << "  row "<< row<<std::endl;
+    std::cout<<"mcp  "<<mcp << " tdcSeq  " <<tdcSeq << " col  "<< col << "  row "<< row<<std::endl;
   }
   
   drawDigi("m,p,v\n",3,max,min);
