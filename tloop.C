8c9265aea1ea64bf8dd8bbd5bf9d06db25746779
Roman Dzhygadlo <r.dzhygadlo@gsi.de>
2015-05-20 14:43:40 +0200

    simple loop thorough T tree

---
+++ tloop.C
@@ -0,0 +1,43 @@
+#define TTSelector_cxx
+#include "prttools.C"
+#include "tloop.h"
+
+TH1F * hEvt1 = new TH1F("hEvt1","hEvt;event [#];counts [#]",10000,0,10000);
+TH1F * hEvt0 = new TH1F("hEvt0","hEvt;event [#];counts [#]",10000,0,10000);
+
+void TTSelector::Begin(TTree *){
+  CreateMap();
+}
+
+Bool_t TTSelector::Process(Long64_t entry){
+  if(entry%1000==0) std::cout<<"event # "<< entry <<std::endl;
+  GetEntry(entry);
+
+  for(Int_t i=0; i<Hits_; i++){
+    Int_t ch = 48*map_tdc[Hits_nTrbAddress[i]]+Hits_nTdcChannel[i];
+    
+    if(ch == 455){
+      if(Hits_nSignalEdge[i]==1)  hEvt1->Fill(entry);
+      if(Hits_nSignalEdge[i]==0)  hEvt0->Fill(entry);
+    }
+  }
+  
+  return kTRUE;
+}
+ 
+void TTSelector::Terminate(){
+  hEvt1->Draw();
+  hEvt0->SetLineColor(2);
+  hEvt0->Draw("same");
+} 
+
+void tloop(TString inFile= "/data.local/may2015/pl15139141259.hld.root"){
+  TChain* ch = new TChain("T");
+  ch->Add(inFile);
+  
+  Int_t entries = ch->GetEntries();
+  TTSelector *selector = new TTSelector();
+  
+  std::cout<<"Entries in chain:  "<< entries<<std::endl;
+  ch->Process(selector,"",entries);
+}
