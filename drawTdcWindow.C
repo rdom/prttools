b1c49cd0233d03c2ce4918a8930324655f2ab57d
rdom <r.dzhygadlo@gsi.de>
2015-07-02 18:57:45 +0200

    time plots for tdc window

---
+++ drawTdcWindow.C
@@ -0,0 +1,26 @@
+#define prt__sim
+#include "../prtdirc/src/PrtHit.h"
+#include "../prtdirc/src/PrtEvent.h"
+#include "prttools.C"
+
+
+void drawTdcWindow(TString infile="hits.root"){
+  //fSavePath = "scan3";
+  PrtInit(infile,1);
+
+  TH1F * hTime[tdcnum];
+  for(Int_t i=0; i<tdcnum; i++) hTime[i]   = new TH1F(Form("time%d",i),Form("time%d",i),1000,-1000,1000);
+  
+  PrtHit fHit;
+  for (Int_t ievent=0; ievent<fCh->GetEntries(); ievent++){
+    PrtNextEvent(ievent,1000);
+    for(Int_t h=0; h<fEvent->GetHitSize(); h++){
+      fHit = fEvent->GetHit(h);
+      hTime[fHit.GetTdc()]->Fill(fHit.GetLeadTime());
+    }
+  }
+  TFile *f = new TFile(infile+".time.root","recreate");
+  for(Int_t i=0; i<tdcnum; i++) hTime[i]->Write();
+  
+  //canvasSave(1,0);
+}
