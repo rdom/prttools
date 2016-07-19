ce400a21cc952eac80c39ca780d47e11f119d61e
Roman Dzhygadlo <r.dzhygadlo@gsi.de>
2016-07-19 19:06:26 +0200

    sync

--- cdisplay.C
+++ cdisplay.C
@@ -435,7 +435,7 @@ void exec3event(Int_t event, Int_t gx, Int_t gy, TObject *selected){
 	  hPiTime[mcp][pix]->SetLineColor(4);
 	  hPiTime[mcp][pix]->Draw("same");
 	}
-	//prt_fit(hh[0],1,1);
+	prt_fit(hPTime[mcp][pix],1,1);
 	//hh[0]->Draw("same");
 	//if(gMode>=100 &&  hh[0]->GetEntries()>10) hh[1]->Draw("same");
       }
