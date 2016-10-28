c300f01bf44de1031791f0a908bf69439deae78a
rdom <r.dzhygadlo@gsi.de>
2016-10-28 12:14:49 +0200

    t1t3h0

--- createPdf.C
+++ createPdf.C
@@ -86,10 +86,10 @@ void createPdf(TString path="/data.local/data/jun15/beam_15183022858C.root", Boo
 	//if(gch>1060)
 	tof2=true;
 
-	if(gch>776 && gch<=780) //4
+	//if(gch>776 && gch<=780) //4
 	//if(gch>777 && gch<779) //2
 	  hodo1=true;
-	if(gch>=790 && gch<794) //4
+	//if(gch>=790 && gch<794) //4
 	//if(gch>791 && gch<793) //2
 	  hodo2=true;
       }
