d00ff48c0915520b3ba2d5ed66be6929b8f9460c
Roman Dzhygadlo <r.dzhygadlo@gsi.de>
2017-02-01 11:57:04 +0100

    sync

--- createPdf.C
+++ createPdf.C
@@ -86,7 +86,7 @@ void createPdf(TString path="/data.local/data/jun15/beam_15183022858C.root", Int
  
        	if(gch==818)
 	  t1=true;
-	if(gch==821)
+	//if(gch==821)
 	  t2=true;
 	if(gch==819)
 	  t3=true;
@@ -99,7 +99,7 @@ void createPdf(TString path="/data.local/data/jun15/beam_15183022858C.root", Int
 	//if(gch>776 && gch<=780) //4
 	//if(gch>777 && gch<779) //2
 	  hodo1=true;
-	if(gch>=791 && gch<=793)
+	if(gch>=790 && gch<=794)
 	  hodo2=true;
       }
 
