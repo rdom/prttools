805f28dd43bac7e06102945408cd97a94a705624
rdom <r.dzhygadlo@gsi.de>
2017-08-29 15:25:03 +0200

    sync

--- tcalibration.C
+++ tcalibration.C
@@ -380,7 +380,7 @@ void tcalibration(TString inFile= "../../data/cj.hld.root", TString outFile= "ou
   gcFile = (cFile!="")? cFile: "0"; // calibration
   gTrigger = trigger;
   gMode = mode;
-  if(gMode == 5) gTrigger=720; //720;
+  if(gMode == 5) gTrigger=816; //720;
   
   TChain* ch = new TChain("T");
   ch->Add(ginFile);
