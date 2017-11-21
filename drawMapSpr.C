308c8532507f6500403572515ec0113e79f9f2a8
Roman Dzhygadlo <r.dzhygadlo@gsi.de>
2017-11-21 21:46:37 +0100

    -sync

--- drawMapSpr.C
+++ drawMapSpr.C
@@ -26,7 +26,35 @@ void drawMapSpr(TString in="tdata/rt.root"){
     hSpr->Fill(test1*1000,test2*1000,spr);
   }
 
-  prt_canvasAdd("hspr",800,500);
+  prt_canvasAdd(Form("hspr_%d",(int)theta),800,500);
   hSpr->Draw("colz");
+  
+  // int binx,biny,binz;  
+  // hSpr->GetBinXYZ(hSpr->GetMinimumBin(),binx,biny,binz);
+  // Double_t binxCenter = hSpr->GetXaxis()->GetBinCenter(binx);
+  // Double_t binyCenter = hSpr->GetYaxis()->GetBinCenter(biny);
+  // TEllipse *el = new TEllipse(binxCenter,binyCenter,.5,.5);
+  // el->Draw();
+  
+  int n =15;
+  double th[]  ={  20,   25,  30,  40,   50,   60,   70,   80,  90,  100,  110,  120,  130,  140,  150};
+  double a[]   ={3.84,-2.38,3.98,1.60,-1.49,-2.60,-0.81,-2.98,0.56,-5.00, 4.59, 3.48, 7.25, 4.69, 1.88};
+  double ph[]  ={4.45,6.11 ,5.36,6.03, 6.13, 3.31, 4.55, 3.14,0.00, -0.5,-5.46,-6.23,-5.29,-5.47,-6.03};
+
+  TGraph *gadiff = new TGraph(n,th,a);
+  TGraph *gphi = new TGraph(n,th,ph);
+  gadiff->SetTitle(";#theta [degree]; #theta - #theta_{fit} [degree]");
+  gadiff->SetMarkerStyle(20);
+  gadiff->SetMarkerSize(0.8);
+  gphi->SetTitle(";#theta [degree]; #varphi_{fit} [degree]");
+  gphi->SetMarkerStyle(20);
+  gphi->SetMarkerSize(0.8);
+
+  prt_canvasAdd("adiff",800,400);
+  gadiff->Draw("APL");
+  prt_canvasAdd("phi",800,400);
+  gphi->Draw("APL");
+  
+  prt_canvasSave();
   
 }
